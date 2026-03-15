a8f94514f30603e719e2ab84c1253ea0e96d06f5
parina004 <parinakparekh@gmail.com>
2026-03-16 03:11:30 +0400

    tested upload

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from backend.app.db.database import engine, Base
 from backend.app.models.clothing_item import ClothingItem
 from fastapi import FastAPI
-from backend.app.api.routes import clothing
+from backend.app.api.routes import clothing,upload
 
 def create_tables():
     Base.metadata.create_all(bind=engine)
@@ -16,6 +16,8 @@ def start():
 
 @app.get("/")
 def test():
-    return {"message":"dressd-ai is running"}
+    return {"message":"dressd-ai server is running"}
 
+#including routers from routes folder
 app.include_router(clothing.router)
+app.include_router(upload.router)
